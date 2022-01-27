2330cfdfc4d8e4cc8bf069d05be72b704169b24b
Aldrich Tambajong <aldricht@Aldrichs-MBP.wifi.gsu.edu>
2022-01-27 12:36:16 -0500

    added base react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path ='' element={<Navigate to="/cover"></Navigate>}></Route>
+
+          <Route path='/cover' element={
+            <div>
+              <h1>Test Page</h1>
+            </div>
+          }></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
