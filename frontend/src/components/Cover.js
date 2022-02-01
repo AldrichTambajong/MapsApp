f992ce7d5cddce422124cf11afc6150ef11b31b0
Aldrich Tambajong <aldricht@Aldrichs-MBP.wifi.gsu.edu>
2022-01-31 21:17:08 -0500

    removed explicit use of API key for maps

--- frontend/src/components/Cover.js
+++ frontend/src/components/Cover.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import {React,useEffect} from 'react';
 import {useNavigate} from "react-router-dom"
 
-function Cover() {
+function Cover(props) {
     let path = useNavigate()
+
+    useEffect(()=> {
+        fetch('/requestKey')
+        .then(response => response.json())
+        .then(data =>{
+            sessionStorage.setItem("apiKey",data.accessKey)
+            props.setApiKey(sessionStorage.getItem("apiKey",data.accessKey))
+        })
+    })
   return (
       <div>
           <div>
