400c8cf4909d77e8577218c3dabfe33050a71cbc
Aldrich Tambajong <aldricht@Aldrichs-MBP.wifi.gsu.edu>
2022-02-01 19:22:16 -0500

    reworked getting current location; added functional polymap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import {React,useState} from 'react';
+import {React,useState,useEffect} from 'react';
 import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom"
 import Cover from './components/Cover';
 import Home from './components/Home'
 
 function App({google}) {
   const [apiKey,setApiKey] = useState(sessionStorage.getItem("apiKey"))
-  
+  const [userCoords,setUserCoords] = useState(JSON.parse(sessionStorage.getItem("userCoords")))
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+          var userLoc = {'lat':position.coords.latitude,'lng':position.coords.longitude}
+      console.log(userLoc)
+      sessionStorage.setItem("userCoords",JSON.stringify(userLoc))
+      setUserCoords(JSON.parse(sessionStorage.getItem("userCoords")))
+      console.log("latitude is: ",position.coords.latitude)
+      console.log("longitude is:",position.coords.longitude)
+      }
+  )
+      fetch('/requestKey')
+      .then(response => response.json())
+      .then(data =>{
+          sessionStorage.setItem("apiKey",data.accessKey)
+          setApiKey(sessionStorage.getItem("apiKey",data.accessKey))
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
   return (
     <div className="App">
       <Router>
@@ -16,9 +35,9 @@ function App({google}) {
         <Routes>
           <Route path='' element={<Navigate to="/cover"></Navigate>}></Route>
 
-          <Route path='/cover' element={<Cover setApiKey={setApiKey}></Cover>}></Route>
+          <Route path='/cover' element={<Cover setUserCoords ={setUserCoords} setApiKey={setApiKey}></Cover>}></Route>
 
-          <Route path='/home' element={<Home google={google} apiKey={apiKey}></Home>}></Route>
+          <Route path='/home' element={<Home google={google} apiKey={apiKey} userCoords={userCoords}></Home>}></Route>
         </Routes>
       </Router>
     </div>
