489579879ba6be5573339634945698915f1250a4
Aldrich Tambajong <aldricht@Aldrichs-MBP.wifi.gsu.edu>
2022-02-03 18:30:32 -0500

    styling for <Map> component

---
+++ frontend/src/style/map_style.js
@@ -0,0 +1,10 @@
+export const style ={
+    maxWidth:'45%',
+    minWidth:'800px',
+    height: "400px",
+  display: "inherit",
+  overflow: "hidden",
+  marginTop:"10px",
+  marginRight: "auto",
+  marginLeft: "auto"
+}
