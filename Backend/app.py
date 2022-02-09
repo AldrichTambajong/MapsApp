98c61af76f8ddd777f072c195b4cbdbfe64aa476
Aldrich Tambajong <aldricht@Aldrichs-MBP.wifi.gsu.edu>
2022-02-08 21:08:49 -0500

    deleted unnecesary comments

--- Backend/app.py
+++ Backend/app.py
@@ -14,13 +14,6 @@ MAPS_KEY = os.getenv("MAPS_KEY")
 
 map_client = googlemaps.Client(MAPS_KEY)
 
-# dest1 = "3973 Ivy Gate Dr, Buford, GA"
-# # All client requests are used if not commented out. Comment out for usage
-# response = map_client.geocode(dest1) 
-# response2 = map_client.distance_matrix("3973 Ivy Gate Dr", "1216 Wildwood Trace",units="imperial")
-# pprint(response)
-
-
 @app.route('/requestKey',methods=["GET","POST"])
 def requestKey():
     key = {
